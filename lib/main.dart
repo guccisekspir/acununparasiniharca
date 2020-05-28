d2fd0ed2ce5cb8bc9fd47dbc681d0aa10e0418e7
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2020-05-28 04:48:21 +0300

    fotoğraf yapısı oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:acununparasiniharca/pages/homePage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 
@@ -11,15 +12,56 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ScrollController _scrollController=ScrollController();
   ScrollController _sscrollController=ScrollController();
+  int _servet=1000000000;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0XFF2c3848),
 
       body: SingleChildScrollView(
         controller: _scrollController,
         child: Column(
           children: [
-            CircleAvatar(child: Icon(Icons.event),radius: 70,),
+            Container(
+              child: Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      width: 160,
+                      height: 160,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [Colors.limeAccent,Colors.cyanAccent]),
+                          color: Colors.limeAccent,
+                          shape: BoxShape.circle),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("assets/acunpp.jpg"),
+                      radius: 70, //TODO radiusları mediaquerye göre yap
+                      backgroundColor: Theme.of(context).accentColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 22,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.limeAccent,
+                ),
+
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Kalan Para "+_servet.toString()+" ₺",style: GoogleFonts.bangers(fontSize: 32),),
+                ),),
+            ),
             SizedBox(height: 30,),
             GridView.builder(
               controller: _sscrollController,
