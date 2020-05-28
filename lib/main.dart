2b76c59da9ace0718b137a6ca006b7f7eaba23a7
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2020-05-28 04:04:51 +0300

    ana yapı oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home:HomePage(),
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -9,8 +9,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ScrollController _scrollController=ScrollController();
+  ScrollController _sscrollController=ScrollController();
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red,);
+    return Scaffold(
+
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Column(
+          children: [
+            CircleAvatar(child: Icon(Icons.event),radius: 70,),
+            SizedBox(height: 30,),
+            GridView.builder(
+              controller: _sscrollController,
+              shrinkWrap: true,
+              itemCount: 10,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+
+              crossAxisCount: 2
+            ), itemBuilder: (context,index){
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(child: Text(index.toString()),),
+              );
+            },)
+          ],
+
+        ),
+      ),
+    );
   }
 }
